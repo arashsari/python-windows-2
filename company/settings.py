9388013e28bac8c3c86eaa1bff0a51cd9def1c4e
Arash (Mehdi) Sarikhani <sarikhani.mehdi@gmail.com>
2023-01-10 09:04:22 +1100

    Update settings.py

--- company/settings.py
+++ company/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=kpli-kjn@mk+-^ng+d%-frxc@c-_qr^i_6(+)m+ytm!11ue6@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get("DJANGO_ALLOWED_HOSTS")]
 
